b0b5c961a8987c4ffe28174f5b3e132d28718caf
dantezhu <dantezhu@qq.com>
2014-10-14 19:06:38 +0800

    eventbus

---
+++ Classes/eventbus/EventBus.cpp
@@ -0,0 +1,71 @@
+﻿#include "EventBus.h"
+
+namespace eventbus {
+
+    bool Event::init()
+    {
+        return true;
+    }
+
+    EventBus::EventBus():m_events(cocos2d::CCArray::create())
+    {
+        m_events->retain();
+    }
+
+    EventBus::~EventBus()
+    {
+        m_events->release();
+    }
+
+    void EventBus::reg(IHandler* handler)
+    {
+        m_handlers.insert(handler);
+    }
+
+    void EventBus::unreg(IHandler* handler)
+    {
+        m_handlers.erase(handler);
+    }
+
+    void EventBus::onEvent(Event* e)
+    {
+        set<IHandler*> handlers=m_handlers;
+
+        while(1) {
+            set<IHandler*> tmp_handler;
+
+            set_intersection(handlers.begin(), handlers.end(),
+                    m_handlers.begin(), m_handlers.end(),
+                    insert_iterator<set<IHandler*> >(tmp_handler, tmp_handler.begin()));
+
+            if (tmp_handler.empty()) {
+                break;
+            }
+
+            auto it = tmp_handler.begin();
+            try{
+                (*it)->onEvent(e);
+            }
+            catch(...)
+            {
+                cout<<"Handler("<<*it<<"):Handler onEvent error"<<endl;
+            }
+
+            handlers.erase(*it);
+        }
+    }
+
+    void EventBus::pushEvent(Event* event_)
+    {
+        m_events->addObject(event_);
+    }
+
+    void EventBus::loopEvents()
+    {
+        for(int i=0;i< m_events->count();++i){
+            onEvent((Event*)m_events->getObjectAtIndex(i));
+        }
+        m_events->removeAllObjects();
+    }
+
+}
---
+++ Classes/eventbus/EventBus.h
@@ -0,0 +1,47 @@
+﻿#ifndef __EVENTBUS_H_20141014190353__
+#define __EVENTBUS_H_20141014190353__
+
+#include <iostream>
+#include <set>
+#include "cocos2d.h"
+
+using namespace std;
+
+namespace eventbus{
+    class Event: public cocos2d::Ref
+    {
+    public:
+        Event(){}
+        virtual ~Event(){}
+
+        virtual bool init();
+        CREATE_FUNC(Event);
+    };
+
+    class IHandler
+    {
+    public:
+        virtual ~IHandler(){}
+        virtual void onEvent(Event*)=0;
+    };
+
+    class EventBus
+    {
+    public:
+        EventBus();
+        ~EventBus();
+        void reg(IHandler*);
+        void unreg(IHandler*);
+        void pushEvent(Event*);
+        void loopEvents();
+    private:
+        void onEvent(Event* e);
+    private:
+        cocos2d::CCArray* m_events;
+        //cocos2d::Vector<IHandler*>* m_events2;
+        set<IHandler*> m_handlers;
+    };
+
+}
+
+#endif /* end of include guard: __EVENTBUS_H_20141014190353__ */
