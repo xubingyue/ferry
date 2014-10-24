0f93fd7dfc599439917487eb9055a07ec1458367
dantezhu <dantezhu@qq.com>
2014-10-24 17:06:28 +0800

    统一用hpp

---
+++ lua/Classes/AppDelegate.cpp
@@ -0,0 +1,31 @@
+/*
+// 在文件中记得引入
+
+#include "lua_ferry_auto.hpp"
+#include "lua_ferry_manual.h"
+
+    // ...
+
+    LuaStack* stack = engine->getLuaStack();
+    stack->setXXTEAKeyAndSign("2dxLua", strlen("2dxLua"), "XXTEA", strlen("XXTEA"));
+    
+    //register custom function
+    //LuaStack* stack = engine->getLuaStack();
+    //register_custom_function(stack->getLuaState());
+
+// ADD-BEGIN by dantezhu in 2014-10-24 17:02:48
+    lua_getglobal(stack->getLuaState(), "_G");
+    register_all_ferry(stack->getLuaState());
+    register_all_ferry_manual(stack->getLuaState());
+    lua_settop(stack->getLuaState(), 0);
+// ADD-END
+
+
+#if (COCOS2D_DEBUG>0)
+    if (startRuntime())
+        return true;
+#endif
+
+    // ....
+    
+*/
