3b351795e962076e796614bdd8b87b2cb6983903
dantezhu <dantezhu@qq.com>
2014-10-26 20:32:19 +0800

    之前queue忘记管deque的线程安全了。

--- cpp/Classes/ferry/BlockQueue.h
+++ cpp/Classes/ferry/BlockQueue.h
@@ -22,8 +22,9 @@ private:
         m_count = 0;
         m_maxsize = maxsize;
 
-        pthread_mutex_init(&m_not_full_mutex, NULL);
-        pthread_mutex_init(&m_not_empty_mutex, NULL);
+        pthread_mutex_init(&m_push_mutex, NULL);
+        pthread_mutex_init(&m_pop_mutex, NULL);
+        pthread_mutex_init(&m_op_mutex, NULL);
         pthread_cond_init(&m_not_full_cond, NULL);
         pthread_cond_init(&m_not_empty_cond, NULL);
     }
@@ -38,8 +39,9 @@ public:
     }
 
     virtual ~BlockQueue() {
-        pthread_mutex_destroy(&m_not_full_mutex);
-        pthread_mutex_destroy(&m_not_empty_mutex);
+        pthread_mutex_destroy(&m_push_mutex);
+        pthread_mutex_destroy(&m_pop_mutex);
+        pthread_mutex_destroy(&m_op_mutex);
         pthread_cond_destroy(&m_not_full_cond);
         pthread_cond_destroy(&m_not_empty_cond);
     }
@@ -74,7 +76,7 @@ public:
 
         int ret;
 
-        pthread_mutex_lock(&m_not_full_mutex);
+        pthread_mutex_lock(&m_push_mutex);
         while (m_maxsize >= 0 && m_count >= m_maxsize) {
             if (block_sec > 0) {
                 struct timespec timeout;
@@ -83,28 +85,31 @@ public:
                 timeout.tv_nsec = 0;
 
                 // 如果要阻塞，就等在这里
-                ret = pthread_cond_timedwait(&m_not_full_cond, &m_not_full_mutex, &timeout);
+                ret = pthread_cond_timedwait(&m_not_full_cond, &m_push_mutex, &timeout);
                 if (ret == ETIMEDOUT) {
                     // 是超时了，并没有等到成功
-                    pthread_mutex_unlock(&m_not_full_mutex);
+                    pthread_mutex_unlock(&m_push_mutex);
                     return ret;
                 }
             }
             else if (block_sec < 0) {
-                pthread_cond_wait(&m_not_full_cond, &m_not_full_mutex);
+                pthread_cond_wait(&m_not_full_cond, &m_push_mutex);
             }
             else {
                 // 返回复制，说明push失败
-                pthread_mutex_unlock(&m_not_full_mutex);
+                pthread_mutex_unlock(&m_push_mutex);
                 return -1;
             }
 
         }
 
+        pthread_mutex_lock(&m_op_mutex);
         m_queue.push_back(d);
         m_count++;
+        pthread_mutex_unlock(&m_op_mutex);
+
         pthread_cond_signal(&m_not_empty_cond);
-        pthread_mutex_unlock(&m_not_full_mutex);
+        pthread_mutex_unlock(&m_push_mutex);
 
         return 0;
     }
@@ -112,7 +117,7 @@ public:
     int pop(DataType &d, int block_sec) {
         int ret;
 
-        pthread_mutex_lock(&m_not_empty_mutex);
+        pthread_mutex_lock(&m_pop_mutex);
         while (m_count <= 0)
             if (block_sec > 0) {
                 struct timespec timeout;
@@ -120,26 +125,29 @@ public:
                 timeout.tv_sec = time(NULL) + block_sec;
                 timeout.tv_nsec = 0;
                 
-                ret = pthread_cond_timedwait(&m_not_empty_cond, &m_not_empty_mutex, &timeout);
+                ret = pthread_cond_timedwait(&m_not_empty_cond, &m_pop_mutex, &timeout);
                 if (ret == ETIMEDOUT) {
                     // 是超时了，并没有等到成功
-                    pthread_mutex_unlock(&m_not_empty_mutex);
+                    pthread_mutex_unlock(&m_pop_mutex);
                     return ret;
                 }
             }
             else if (block_sec < 0) {
-                pthread_cond_wait(&m_not_empty_cond, &m_not_empty_mutex);
+                pthread_cond_wait(&m_not_empty_cond, &m_pop_mutex);
             }
             else {
-                pthread_mutex_unlock(&m_not_empty_mutex);
+                pthread_mutex_unlock(&m_pop_mutex);
                 return -1;
             }
 
+        pthread_mutex_lock(&m_op_mutex);
         d = m_queue.front();
         m_queue.pop_front();
         m_count--;
+        pthread_mutex_unlock(&m_op_mutex);
+
         pthread_cond_signal(&m_not_full_cond);
-        pthread_mutex_unlock(&m_not_empty_mutex);
+        pthread_mutex_unlock(&m_pop_mutex);
 
         return 0;
 
@@ -160,9 +168,16 @@ public:
 private:
     int m_count;
     int m_maxsize;
-    pthread_mutex_t m_not_full_mutex;
-    pthread_mutex_t m_not_empty_mutex;
+    // push 锁
+    pthread_mutex_t m_push_mutex;
+    // pop 锁
+    pthread_mutex_t m_pop_mutex;
+    // 操作 锁
+    pthread_mutex_t m_op_mutex;
+
+    // 不满 条件
     pthread_cond_t m_not_full_cond;
+    // 不空 条件
     pthread_cond_t m_not_empty_cond;
 
     std::deque<DataType> m_queue;
