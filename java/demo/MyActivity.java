ecfd8893c16e5e18a36b6425e45f209887f00f6e
dantezhu <dantezhu@qq.com>
2016-03-14 18:50:03 +0800

    demo改一下

--- java/demo/MyActivity.java
+++ java/demo/MyActivity.java
@@ -61,7 +61,7 @@ public class MyActivity extends Activity {
                     }
 
                     @Override
-                    public void onTimeout() {
+                    public void onTimeout(int code, IBox ibox) {
                         Log.d(LOG_TAG_RSP, "onTimeout");
                     }
                 }, 5, this);
